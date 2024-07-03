073fea20d0fe409e0f388ceb398203aa7b497d68
Ethan Chen <ethan.chen@hyland.com>
2024-07-03 15:22:25 -0400

    basic hyland website is complete still needs extra work though

--- script.js
+++ script.js
@@ -21,3 +21,22 @@ function web102(url) {
 function web103(url) {
   window.location.href = url;
 }
+
+
+let slideIndex = 1;
+showSlides(slideIndex);function plusSlide(n) {
+  showSlides(slideIndex += n);
+}function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}function prevSlide() {
+  plusSlide(-1);
+}function nextSlide() {
+  plusSlide(1);
+}
