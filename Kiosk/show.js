d62adc693c90b7b2ed4cd9454a8e8fd67cdab40b
Joseph Maxwell <joseph.maxwell@hyland.com>
2026-04-07 15:14:09 -0400

    kiosk 47

---
+++ Kiosk/show.js
@@ -0,0 +1,55 @@
+const show = {
+  marqueeText: "welcome to hy-tech club week 5  🖥️  hylandtechclub.com  🍕  take a seat + grab some pizza  🔳  solve nonogram puzzles  🖥️  hylandtechclub.com  🖥️",
+  slides: [
+    {
+      backgroundImage: "Assets/BackgroundImages/pixelrobot.gif",
+    },
+    {
+      title: "💻 HYLANDTECHCLUB.COM 💻",
+      subtitle: "one stop shop",
+      content: ``,
+      backgroundImage: "./Assets/BackgroundImages/animeatcomputer.gif",
+      backgroundSize: "contain !important",
+      backgroundOpacity: ".3",
+      backgroundColor: "white",
+      textColor: "black"
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/sailormoon.gif",
+      backgroundSize: "contain !important",
+    },
+    {
+      title: "😔 NO WI-FI 😔",
+      subtitle: "sorry guys",
+      content: ``,
+      backgroundImage: "./Assets/BackgroundImages/headdown.gif",
+      backgroundSize: "contain !important",
+      backgroundOpacity: ".3",
+      backgroundColor: "#88aaff",
+      textColor: "#772222"
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/beachsunset.webp",
+      backgroundOpacity: ".5",
+      backgroundColor: "pink",
+      content: `<img src="Assets/ForegroundImages/FOllowDreams.jpg">`
+    },
+    {
+      title: "🔳 NONOGRAM PUZZLES 🔳",
+      subtitle: "fill the grid to find the pictures",
+      content: `<div style="display: flex; gap: 20px; justify-content: center; align-items: flex-start;">
+      <img src="Assets/ForegroundImages/nonohow.gif"><img src="Assets/ForegroundImages/nonohow2.gif" style="height:500px;">`,
+      backgroundImage: "",
+      backgroundSize: "contain !important",
+      backgroundOpacity: ".3",
+      backgroundColor: "white",
+      textColor: "black"
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/beachsunset.webp",
+      backgroundOpacity: ".5",
+      backgroundColor: "purple",
+      content: `<img src="Assets/ForegroundImages/Mission.jpg">`
+    },
+  ]
+};
