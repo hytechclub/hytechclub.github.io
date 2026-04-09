84dd250658bdce0dd8951c7d849da6f890939d87
Joseph Maxwell <joseph.maxwell@hyland.com>
2026-04-09 14:53:36 -0400

    mcccc 49

---
+++ Kiosk/shows/mcccc4926.js
@@ -0,0 +1,56 @@
+const show = {
+  marqueeText: "🖥️  welcome to mcccc  🍕  pizza & chicken @ 5pm  💺  take a seat anywhere  ⚙️  emerging tech content @ 5:30  ",
+  slides: [
+    {
+      backgroundImage: "Assets/BackgroundImages/pixelrobot.gif",
+    },
+    {
+      title: "💻 PC CREDENTIALS 💻",
+      subtitle: "MidtownUser / Midtown12345",
+      content: ``,
+      backgroundImage: "./Assets/BackgroundImages/animeatcomputer.gif",
+      backgroundSize: "contain !important",
+      backgroundOpacity: ".3",
+      backgroundColor: "white",
+      textColor: "black"
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/sailormoon.gif",
+      backgroundSize: "contain !important",
+    },
+    {
+      title: "🛜 WI-FI: MCC-PUBLIC 🛜",
+      subtitle: "open browser to connect",
+      content: ``,
+      backgroundImage: "./Assets/BackgroundImages/hackcodefalling.gif",
+      backgroundSize: "contain !important",
+      backgroundOpacity: ".4",
+      backgroundColor: "#005500",
+      textColor: "white"
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/beachsunset.webp",
+      backgroundOpacity: ".5",
+      backgroundColor: "pink",
+      content: `<img src="Assets/ForegroundImages/FOllowDreams.jpg">`
+    },
+    {
+      title: "🎫 TODAY'S RAFFLE 🎫",
+      subtitle: "earn tickets throughout, win amazing prizes",
+      content: `<div style="display: flex; gap: 10px; justify-content: center; align-items: flex-start; height: 50vh;">
+      <img src="Assets/ForegroundImages/needoh.webp" style="height:100%"><img src="Assets/ForegroundImages/moley.jpg" style="height:100%;">
+      <img src="Assets/ForegroundImages/pokemoncards.avif" style="height:100%">`,
+      backgroundImage: "",
+      backgroundSize: "contain !important",
+      backgroundOpacity: ".3",
+      backgroundColor: "white",
+      textColor: "black"
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/beachsunset.webp",
+      backgroundOpacity: ".5",
+      backgroundColor: "purple",
+      content: `<img src="Assets/ForegroundImages/Mission.jpg">`
+    }
+  ]
+};
