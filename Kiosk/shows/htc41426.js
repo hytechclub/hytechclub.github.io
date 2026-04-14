08a8b4525ae739fc767976fb9675d3c3a31aaeb9
Joseph Maxwell <joseph.maxwell@hyland.com>
2026-04-14 15:04:25 -0400

    new one

---
+++ Kiosk/shows/htc41426.js
@@ -0,0 +1,58 @@
+const show = {
+  marqueeText: "welcome to hy-tech club week 6  🖥️  hylandtechclub.com  🍕  take a seat + grab some pizza  💡  activity coming soon  🖥️  hylandtechclub.com  🖥️",
+  slides: [
+    {
+      backgroundImage: "Assets/BackgroundImages/pixelcomputeranime.gif",
+    },
+    {
+      title: "💻 HYLANDTECHCLUB.COM 💻",
+      subtitle: "",
+      content: ``,
+      backgroundImage: "./Assets/BackgroundImages/animeoldpc2.gif",
+      // backgroundSize: "contain !important",
+      backgroundOpacity: ".3",
+      backgroundColor: "white",
+      textColor: "black"
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/animesky.gif",
+      backgroundPosition: "bottom",
+    },
+    {
+      title: "😔 STILL NO WI-FI 😔",
+      subtitle: "",
+      content: ``,
+      backgroundImage: "./Assets/BackgroundImages/internetconnect.gif",
+      backgroundSize: "contain !important",
+      backgroundOpacity: ".3",
+      backgroundColor: "white",
+      textColor: "red"
+    },
+    {
+      backgroundImage: "",
+      backgroundOpacity: ".5",
+      backgroundColor: "pink",
+      content: `<img src="Assets/ForegroundImages/magic.gif">`
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/mariorunglitch.gif",
+      backgroundPosition: "bottom",
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/beachsunset.webp",
+      backgroundOpacity: ".5",
+      backgroundColor: "pink",
+      content: `<img src="Assets/ForegroundImages/dress.webp">`
+    },   
+    {
+      title: "⭐ STUDENT OF THE MONTH ⭐",
+      subtitle: "announcing soon!",
+      content: ``,
+      backgroundImage: "Assets/BackgroundImages/award.gif",
+      // backgroundSize: "contain !important",
+      backgroundOpacity: ".5",
+      backgroundColor: "#ffffaa",
+      textColor: "black"
+    },
+  ]
+};
