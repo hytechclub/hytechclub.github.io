1776061f78622adcfce6364ff146c64aa0781666
Joseph Maxwell <joseph.maxwell@hyland.com>
2026-04-16 14:16:15 -0400

    april 16 2026

---
+++ Kiosk/shows/mcccc41626.js
@@ -0,0 +1,67 @@
+const show = {
+  marqueeText: "🖥️  welcome to mcccc  🍕  pizza hut @ ~5pm  💺  take a seat anywhere  🎮  play some games",
+  slides: [
+    {
+      backgroundImage: "Assets/BackgroundImages/pixelcomputeranime.gif",
+    },
+    {
+      title: "💻 PC CREDENTIALS 💻",
+      subtitle: "MidtownUser / Midtown12345",
+      content: ``,
+      backgroundImage: "./Assets/BackgroundImages/keyboardanime.gif",
+      backgroundSize: "",
+      backgroundOpacity: ".3",
+      backgroundColor: "white",
+      textColor: "black"
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/animerainfrogs.gif",
+      backgroundSize: "",
+      backgroundPosition: "center",
+    },
+    {
+      title: "🛜 WI-FI: MCC-PUBLIC 🛜",
+      subtitle: "open browser to connect",
+      content: ``,
+      backgroundImage: "./Assets/BackgroundImages/freewifi.webp",
+      backgroundSize: "contain !important",
+      backgroundOpacity: ".3",
+      backgroundColor: "white",
+      textColor: "black"
+    },
+    {
+      backgroundImage: "",
+      backgroundOpacity: ".5",
+      backgroundColor: "pink",
+      content: `<img src="Assets/ForegroundImages/magic.gif" style="height: 75vh;">`
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/mariorunglitch.gif",
+      backgroundPosition: "bottom",
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/beachsunset.webp",
+      backgroundOpacity: ".5",
+      backgroundColor: "pink",
+      content: `<img src="Assets/ForegroundImages/dress.webp">`
+    },   
+    {
+      title: "🎫 TODAY'S RAFFLE 🎫",
+      subtitle: "play games to win tickets",
+      content: `<div style="display: flex; gap: 10px; justify-content: center; align-items: flex-start; height: 50vh;">
+      <img src="Assets/ForegroundImages/needoh.webp" style="height:100%"><img src="Assets/ForegroundImages/ding2.webp" style="height:100%;">
+      <img src="Assets/ForegroundImages/pokemoncards.avif" style="height:100%">`,
+      backgroundImage: "",
+      backgroundSize: "contain !important",
+      backgroundOpacity: ".3",
+      backgroundColor: "white",
+      textColor: "black"
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/beachsunset.webp",
+      backgroundOpacity: ".5",
+      backgroundColor: "purple",
+      content: `<img src="Assets/ForegroundImages/wizardpc.webp" style="height: 75vh;">`
+    },
+  ]
+};
