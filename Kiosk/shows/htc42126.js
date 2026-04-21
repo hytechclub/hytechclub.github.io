fb186a2be5f7bfa09f070af6b694666fbd56eb38
Joseph Maxwell <joseph.maxwell@hyland.com>
2026-04-21 14:08:14 -0400

    ok april 21

---
+++ Kiosk/shows/htc42126.js
@@ -0,0 +1,57 @@
+const show = {
+  marqueeText: "welcome to hy-tech club 🖥️  week 7  🍕  wi-fi: Hy-Tech Club / Welcome2Hyland!2026  🖥️",
+  slides: [
+    {
+      backgroundImage: "Assets/BackgroundImages/wave.gif",
+      backgroundSize: "contain !important",
+    },
+    {
+      title: "💻 HYLANDTECHCLUB.COM 💻",
+      subtitle: "",
+      content: ``,
+      backgroundImage: "./Assets/BackgroundImages/catcomputer.webp",
+      backgroundOpacity: ".3",
+      backgroundColor: "white",
+      textColor: "black"
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/animeblossoms.gif",
+      backgroundPosition: "top",
+    },
+    {
+      title: "🛜 WI-FI INFO 🛜",
+      subtitle: "",
+      content: `<p class="more">Hy-Tech Club<br><b>Welcome2Hyland!2026</b></p>`,
+      backgroundImage: "./Assets/BackgroundImages/noise.gif",
+      backgroundOpacity: ".3",
+      backgroundColor: "white",
+      textColor: "black"
+    },
+    {
+      backgroundImage: "",
+      backgroundOpacity: ".5",
+      backgroundColor: "orange",
+      content: `<img style="height: 80%" src="Assets/ForegroundImages/basketballhead.gif">`
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/tombraider.gif",
+      backgroundPosition: "center",
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/realoldcomputer.png",
+      backgroundOpacity: ".7",
+      backgroundColor: "lime",
+      content: `<img src="Assets/ForegroundImages/learnings.gif">`
+    },   
+    {
+      title: "🍕 PIZZA 🍕",
+      subtitle: "mangia mangia!",
+      content: ``,
+      backgroundImage: "Assets/ForegroundImages/pizzaspin.gif",
+      // backgroundSize: "contain !important",
+      backgroundOpacity: ".5",
+      backgroundColor: "#ffddaa",
+      textColor: "black"
+    },
+  ]
+};
