9936bdd9d4563653ff93ba6b99eb897e69df7eb4
Joseph Maxwell <joseph.maxwell@hyland.com>
2026-04-30 14:41:28 -0400

    ok try again

---
+++ Kiosk/shows/mcccc43026.js
@@ -0,0 +1,59 @@
+const show = {
+  marqueeText: "mcccccccccccccc  🖥️  week 8  💺  feel free to sit anywhere  📖  free study  🍕  little caesar's @ ~5pm",
+  slides: [
+    {
+      backgroundImage: "Assets/BackgroundImages/zoomyshapes.gif",
+      backgroundSize: "contain !important",
+    },
+    {
+      title: "💻 PC CREDENTIALS 💻",
+      subtitle: "MidtownUser / Midtown12345",
+      content: ``,
+      backgroundImage: "./Assets/BackgroundImages/catonpc.webp",
+      backgroundOpacity: ".3",
+      backgroundColor: "white",
+      textColor: "black"
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/fallingstars.gif",
+      backgroundPosition: "top",
+    },
+    {
+      title: "🛜 WI-FI: MCC-PUBLIC 🛜",
+      subtitle: "",
+      content: `<p class="more">open browser to connect</p>`,
+      backgroundImage: "./Assets/BackgroundImages/pixelcolornoise.gif",
+      backgroundOpacity: ".3",
+      backgroundColor: "white",
+      textColor: "black"
+    },
+    {
+      backgroundImage: "",
+      backgroundOpacity: ".5",
+      backgroundColor: "white",
+      content: `<img style="height: 80%" src="Assets/ForegroundImages/pixelgraphs.gif">`
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/cursorcamp.gif",
+      backgroundPosition: "center",
+      title: "",
+      subtitle: "",
+      textColor: "white",
+      backgroundColor: "black",
+      backgroundOpacity: ".4",
+      content: `<p>new NEAL.FUN game! explore the world as a cursor!</p><h1 style="color:cyan;font-style:italic;">neal.fun/cursor-camp</h1>`
+    },
+    {
+      backgroundImage: "",
+      backgroundOpacity: ".7",
+      backgroundColor: "black",
+      content: `<img height="95%" style="top:-40px;" src="Assets/BackgroundImages/facecycle.gif">`
+    },   
+    {
+      backgroundImage: "Assets/BackgroundImages/beachsunset.webp",
+      backgroundOpacity: ".5",
+      backgroundColor: "seagreen",
+      content: `<img src="Assets/ForegroundImages/positive.webp" style="height: 75vh;">`
+    },
+  ]
+};
