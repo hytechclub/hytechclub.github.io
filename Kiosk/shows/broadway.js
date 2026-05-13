db991a124cfb5e02f0896ad828d0bffe7a5078e3
Joseph Maxwell <joseph.maxwell@hyland.com>
2026-05-13 10:10:54 -0400

    good enough

---
+++ Kiosk/shows/broadway.js
@@ -0,0 +1,30 @@
+const show = {
+  marqueeText: "🖥️  welcome to HYLAND at the MCC!  💺  take a seat anywhere  ⚙️  we will begin shortly",
+  slides: [
+    {
+      backgroundImage: "Assets/BackgroundImages/pixelrobot.gif",
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/sailormoon.gif",
+      backgroundSize: "contain !important",
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/realoldcomputer.png",
+      backgroundOpacity: ".7",
+      backgroundColor: "lime",
+      content: `<img src="Assets/ForegroundImages/learnings.gif">`
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/mariorunglitch.gif",
+      backgroundPosition: "bottom",
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/tombraider.gif",
+      backgroundPosition: "center",
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/Wave.gif",
+      backgroundSize: "contain !important",
+    },
+  ]
+};
