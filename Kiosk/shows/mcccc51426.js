3b609100e2f0f17f8f784fc47ed265c665f2e761
Joseph Maxwell <joseph.maxwell@hyland.com>
2026-05-14 15:11:17 -0400

    may 14

---
+++ Kiosk/shows/mcccc51426.js
@@ -0,0 +1,65 @@
+const show = {
+  marqueeText: "mcccccccccccccc  🖥️  week 10  💺  feel free to sit anywhere  🍕  little caesar's @ ~5pm  🎶  music content begins at 5:30pm",
+  slides: [
+    {
+      backgroundImage: "Assets/BackgroundImages/nightdrive.gif",
+      backgroundPosition: "bottom",
+    },
+    {
+      title: "💻 PC CREDENTIALS 💻",
+      subtitle: "MidtownUser / Midtown12345",
+      content: ``,
+      backgroundImage: "./Assets/BackgroundImages/pixelpcgif.gif",
+      backgroundOpacity: ".3",
+      backgroundSize: "contain !important",
+      backgroundColor: "black",
+      textColor: "yellow"
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/jakethedogheadphones.gif",
+      backgroundPosition: "top",
+    },
+    {
+      title: "🛜 WI-FI: MCC-PUBLIC 🛜",
+      subtitle: "",
+      content: `<p class="more">open browser to connect</p>`,
+      backgroundImage: "./Assets/BackgroundImages/pixelcolornoise.gif",
+      backgroundOpacity: ".3",
+      backgroundColor: "white",
+      textColor: "blue"
+    },
+    {
+      backgroundImage: "",
+      backgroundOpacity: ".5",
+      backgroundColor: "white",
+      content: `<img style="height: 80%" src="Assets/ForegroundImages/enjoybeing.webp">`
+    },
+    {
+      subtitle: "me when i have to go to space",
+      textColor: "white",
+      backgroundImage: "Assets/BackgroundImages/spacebugs.gif",
+      backgroundPosition: "center",
+    },
+    {
+      title: "🥈 SECOND-TO-LAST WEEK 🥈",
+      subtitle: "one more after this",
+      backgroundImage: "Assets/BackgroundImages/retroscreen.gif",
+      backgroundOpacity: ".5",
+      backgroundColor: "black",
+      textColor: "silver",
+      content: `<div style="background-color: white; color: black; width:590px;border:5px solid black; padding:20px;margin:0 auto; text-align: left"><p><b>this week:</b><br> music</p><p><b>next week (may 21):</b> game design</p><p><b>week after that:</b></p></div> `
+    },
+    {
+      backgroundImage: "",
+      backgroundOpacity: ".7",
+      backgroundColor: "black",
+      content: `<img height="95%" style="top:-40px;" src="Assets/ForegroundImages/breatheinandout.webp">`
+    },
+    {
+      backgroundImage: "",
+      backgroundOpacity: ".7",
+      backgroundColor: "blue",
+      content: `<img height="95%" style="top:-40px;" src="Assets/BackgroundImages/dolphins.gif">`
+    },
+  ]
+};
