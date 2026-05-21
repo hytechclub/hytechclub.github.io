ccca7913bb8e5a74121bb60b8ad81c38ffd85624
Joseph Maxwell <joseph.maxwell@hyland.com>
2026-05-21 13:36:15 -0400

    may 21

---
+++ Kiosk/shows/mcccc52126.js
@@ -0,0 +1,91 @@
+const show = {
+  marqueeText: "mcccc  😭  final week of spring 2026  🍕  rascal house pizza & chicken @ ~5pm  🎮  game design content begins at 5:30pm  🖥️",
+  slides: [
+    {
+      backgroundImage: "Assets/BackgroundImages/animeoldpc.gif",
+      backgroundPosition: "bottom"
+    },
+    {
+      title: "💻 PC CREDENTIALS 💻",
+      subtitle: "MidtownUser / Midtown12345",
+      content: ``,
+      backgroundImage: "./Assets/BackgroundImages/animecomputers.gif",
+      backgroundOpacity: ".8",
+      backgroundSize: "contain !important",
+      backgroundColor: "black",
+      textColor: "white"
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/colorfulold.gif",
+      backgroundSize: "contain !important",
+    },
+    {
+      subtitle: "daily motivation",
+      backgroundImage: "Assets/BackgroundImages/beachsunset.webp",
+      backgroundOpacity: ".5",
+      backgroundColor: "skyblue",
+      textColor: "white",
+      content: `<img style="height: 80%" src="Assets/ForegroundImages/justdo.webp">`
+    },
+    {
+      title: "👕 FREE T-SHIRTS 👕",
+      subtitle: "back of the room - first come first serve",
+      backgroundImage: "./Assets/BackgroundImages/animeskythisone.gif",
+      backgroundOpacity: ".3",
+      backgroundColor: "#d0eeff",
+      backgroundPosition: "top",
+      textColor: "black",
+      content: `<img style="height: 70%" src="Assets/ForegroundImages/freeshirts.jpeg">`
+    },
+    {
+      backgroundImage: "Assets/BackgroundImages/flowers.gif",
+    },
+    {
+      backgroundImage: "",
+      backgroundOpacity: ".5",
+      backgroundColor: "white",
+      content: `
+    <div class="side-by">
+        <h1>🥲 <br>FINAL SESSION OF THE SEASON<br>🥲 </h1>
+        <img style="height: 100%;" src="Assets/ForegroundImages/thanksforeverything.webp">
+    </div>`
+    },
+    
+    {
+      subtitle: "today is almost over",
+      backgroundImage: "Assets/BackgroundImages/noise.gif",
+      backgroundOpacity: ".2",
+      backgroundColor: "#440000",
+      backgroundSize: "contain !important",
+      textColor: "white",
+      content: `<img style="height: 80%" src="Assets/ForegroundImages/friyay.webp">`
+    },
+    {
+      title: "🛜 WI-FI: MCC-PUBLIC 🛜",
+      subtitle: "",
+      content: `<p class="more">open browser to connect</p>`,
+      backgroundImage: "./Assets/ForegroundImages/uhhinternet.gif",
+      backgroundSize: "contain !important",
+      backgroundPosition: "center",
+      backgroundOpacity: ".5",
+      backgroundColor: "#330000",
+      textColor: "white"
+    },
+    
+    {
+      title: "FUN FACT OF THE DAY",
+      backgroundImage: "",
+      backgroundOpacity: ".2",
+      backgroundColor: "gray",
+      backgroundSize: "contain !important",
+      textColor: "white",
+      content: `<img style="height: 80%" src="Assets/ForegroundImages/funfact.webp">`
+    },
+    {
+      subtitle: "have a great summer!",
+      textColor: "white",
+      backgroundImage: "Assets/BackgroundImages/animesummer.gif",
+      backgroundPosition: "center",
+    },
+  ]
+};
